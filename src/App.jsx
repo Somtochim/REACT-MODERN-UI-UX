106d58b215dfd4c5e528338fe7fd607bed81f85a
Somtochim Oguchi <oguchisomtochim04@gmail.com>
2026-02-04 17:41:16 +0100

    feat: add Notification, Section, and Services components; implement design elements for UI feat: create design components including Benefits, Collaboration, Header, Hero, Pricing, Roadmap, and Services chore: add constants for navigation, services, pricing, and benefits style: set up Tailwind CSS configuration and global styles build: initialize Vite configuration for React application

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Benefits from './components/Benefits'
+import Collaboration from './components/Collaboration'
+//import Button from './components/Button'
+import  {Header}  from './components/Header'
+import Hero from './components/Hero'
+import Services from './components/Services'
+// import Section from './components/Section'
+
+function App() {
+
+
+  return (
+  <div>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+     <Header/>
+     <Hero/>
+     <Benefits/>
+     <Collaboration/>
+     <Services />
+    </div>
+
+    <ButtonGradient />
+         
+    
+  </div>
+  )
+}
+
+export default App
+ 
