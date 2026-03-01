1656cf0c985345c856d60bb25bb32b9ce30d07c6
Somtochim Oguchi <oguchisomtochim04@gmail.com>
2026-03-01 17:30:47 +0100

    fix: update import path for benefits to use the correct index file

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,6 +1,6 @@
 import Heading from "./Heading"
 import Section from "./Section"
-import { benefits } from "../constants"
+import { benefits } from "../constants/index"
 import Arrow from "../assets/svg/Arrow"
 import {GradientLight} from './design/Benefits'
 import ClipPath from '../assets/svg/ClipPath'
