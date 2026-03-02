209b4e39867af36f438cfff6d1cc28901ef39467
Utojiuba Chidera <163609502+Prime-02@users.noreply.github.com>
2026-03-02 11:38:42 +0100

    Update PricingList.jsx

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -7,8 +7,8 @@ import { check } from "../assets"
 const PricingList = () => {
   return (
     <div className="flex gap-[1rem] max-lg:flex-wrap">
-        {pricing.map((item) => (
-            <div id={item.id} className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2  [&>h4]:even:text-color-1 [&>h4]:last:text-color-3">
+        {pricing.map((item, i) => (
+            <div id={i} className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2  [&>h4]:even:text-color-1 [&>h4]:last:text-color-3">
               <h4 className="h4 mb-4 ">{item.title}</h4>
               <p className="body-2 min-h-[4rem] mb-3 text-n-1/50">{item.description}</p>
               <div className="flex items-center h-[5.5rem]">
